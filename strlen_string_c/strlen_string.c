5dfd02bfce4776a5a92b8b551879685f7a1a436a
yangang <yg0103@woozoom.net>
2017-11-04 09:10:49 +0800

    add str function

--- strlen_string_c/strlen_string.c
+++ strlen_string_c/strlen_string.c
@@ -1,14 +1,16 @@
-/**
-  字符串是以\0结束,但是字符串的长度不包括\0, 比如字符串是"abcde"
-,那么字符串的长度就是5
-*/
-
 #include <string.h>
 #include <stdio.h>
 
+void send_char(char *p)
+{
+   printf("char: 0x%x\r\n", *p);
+}
+
 int main()
 {
-   char a[10]="hello";
+    char a[10]="hello";
     int  len = strlen(a);
 	printf("len = %d\r\n", len);
+  
+    send_char((char *)0x1B);
 }
