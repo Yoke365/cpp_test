a81fddabe11d8c98f2bb868d1fbfe8633464a7e8
yangang <320054593@qq.com>
2018-04-30 19:18:17 +0800

    update cct

--- cct/main.c
+++ cct/main.c
@@ -1,19 +1,13 @@
 
 #include <stdio.h>
 #include <stdint.h>
-#include "wheel_mesg.h"
 #include "manual_ctrl.h"
-/**
-    wheel_send_mesg : 发送消息
-    wheel_recv_mesg : 接收消息
-*/
-wheel_mesg_s wheel_send_mesg; 
-wheel_mesg_s wheel_recv_mesg;
+
 
 int main(int argc, char argv[])
 {    
-    cct_test(100);
-    //cct_test(1);
+    //cct_test(100);
+    cct_test(5);
 
 	return 0;
 }
