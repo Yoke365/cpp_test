03400950a1221cc9d14bc3f11bb73053cf605305
yangang <yg0103@woozoom.net>
2018-05-09 20:32:50 +0800

    修改cct的合并信息

--- cct/main.c
+++ cct/main.c
@@ -1,7 +1,6 @@
 
 #include <stdio.h>
 #include <stdint.h>
-<<<<<<< HEAD
 #include "wheel_mesg.h"
 #include "manual_ctrl.h"
 /**
@@ -15,15 +14,6 @@ int main(int argc, char argv[])
 {    
     cct_test(100);
     cct_test(1);
-=======
-#include "manual_ctrl.h"
-
-
-int main(int argc, char argv[])
-{    
-    //cct_test(100);
-    cct_test(5);
->>>>>>> 5cb9626e33b36eae2401fed794d44b7978f2d18d
 
 	return 0;
 }
