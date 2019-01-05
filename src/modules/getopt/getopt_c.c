fd622abd0dfec418e59c6b1be762a54e4f5a11ba
yg0103 <320054593@qq.com>
2019-01-05 02:28:41 -0800

    测试getopt1

---
+++ src/modules/getopt/getopt_c.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int
+ getopt_main(int argc, char *argv[])
+{
+	//enum MPU6000_BUS busid = MPU6000_BUS_ALL;
+	//int device_type = MPU_DEVICE_TYPE_MPU6000;
+	int ch;
+	//enum Rotation rotation = ROTATION_NONE;
+	int accel_range = 8;
+    int	device_type;
+	/* jump over start/off/etc and look at options first */
+	while ((ch = getopt(argc, argv, "p:")) != EOF) {
+		// printf("ch：　％c", ch);
+		switch (ch) {
+		case 'X':
+		    //	busid = MPU6000_BUS_I2C_EXTERNAL;
+            // printf("ch：　％c", ch);
+			break;
+
+		case 'I':
+		//	busid = MPU6000_BUS_I2C_INTERNAL;
+			break;
+
+		case 'S':
+		   //	busid = MPU6000_BUS_SPI_EXTERNAL1;
+			break;
+
+		case 's':
+		   //	busid = MPU6000_BUS_SPI_INTERNAL1;
+			break;
+
+		case 'Z':
+			//busid = MPU6000_BUS_SPI_EXTERNAL2;
+			break;
+
+		case 'z':
+		   //	busid = MPU6000_BUS_SPI_INTERNAL2;
+			break;
+
+		case 'p':
+		    	device_type = atoi(optarg);
+			printf("p: %d\r\n", device_type);
+			break;
+
+		case 'R':
+	    	//	rotation = (enum Rotation)atoi(optarg);
+			printf("R: ％s\r\n", optarg);
+			break;
+
+		case 'a':
+		   // accel_range = atoi(optarg);
+			break;
+
+		default:
+		    //mpu6000::usage();
+			exit(0);
+		}
+	}
+
+
+	const char *verb = argv[optind];
+
+	/*
+	 * Start/load the driver.
+
+	 */
+	//if (!strcmp(verb, "start")) {
+		//mpu6000::start(busid, rotation, accel_range, device_type);
+	//	exit(0);  //normal exit
+	//}
+    
+	exit(0);
+}
